8135c97ba5af06d1eeca424c7bfb72e108f256a2
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2020-11-07 15:52:00 +0530

    Add files via upload

---
+++ src/main/java/java8/diff/employeeApp/Main.java
@@ -0,0 +1,13 @@
+package java8.diff.employeeApp;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		// Write your code here
+	}
+}
